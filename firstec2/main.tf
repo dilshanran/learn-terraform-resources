db8017ad47a6d531d0fdaadb7fd452011b38a508
dilshanran <drmanawadu071@gmail.com>
2025-05-13 23:41:15 +0530

    tested

--- firstec2/main.tf
+++ firstec2/main.tf
@@ -11,9 +11,22 @@ provider "aws" { # AWS provider configuration
   region = "us-east-1"
 }
 
-resource "aws_instance" "MWFirstEC2" { # Resource block to create an EC2 instance
-  ami           = "ami-0f88e80871fd81e91" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = "vpc-005cd965"  # Replace with your VPC ID
+  cidr_block              = "172.30.4.0/24"  # Subnet CIDR block
+ # availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+resource "aws_instance" "MWFirstEC2" {    # Resource block to create an EC2 instance
+  ami                    = "ami-0f88e80871fd81e91" # Amazon Linux 2 AMI
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-3d64bf4a"
+ # availability_zone      = "us-east-1c"
 
   tags = {
     Name = "MWFirstEC2"
