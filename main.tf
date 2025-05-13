a87dfef27de83501381c977b5da8a52baf209f85
dilshanran <drmanawadu071@gmail.com>
2025-05-13 16:42:53 +0530

    Fileadded

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.name]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-Demo-Instance"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH access"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # You can restrict this to your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  default     = "ami-0c55b159cbfafe1f0"  # Update if needed
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
+  type = string
+}
